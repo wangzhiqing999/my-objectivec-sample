dc56d6d948984be9b445d41b00273252bc9b7b03
wangzhiqing <wangzhiqing@FoxMacBookAir.local>
2013-07-27 20:16:41 +0800

    Initial Commit

---
+++ my-objectivec-sample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  my-objectivec-sample
+//
+//  Created by wangzhiqing on 13-7-27.
+//  Copyright (c) 2013年 wangzhiqing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
